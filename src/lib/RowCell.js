3565d4861b4b22b2486cc441e8e806776c1f7eb6
itjayaprakash <jprakashrpm@gmail.com>
2020-04-20 11:47:06 +0530

    ARIA-EXPAND

--- src/lib/RowCell.js
+++ src/lib/RowCell.js
@@ -49,12 +49,12 @@ class RowCell extends Component {
     let expandToggleIcon = null;
     if (showExpandCollapse && !isExpanded) {
       expandToggleIcon =
-        <button className="icon-expand" aria-expand="false" onClick={onExpandToggle}>
+        <button className="icon-expanded" aria-expanded="false" onClick={onExpandToggle}>
           <svg height="24" viewBox="0 0 24 24" width="24"><path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z" /><path d="M0 0h24v24H0z" fill="none" /></svg>
         </button>;
     } else if (showExpandCollapse && isExpanded) {
       expandToggleIcon =
-        <button className="icon-collasped" aria-expand="true" onClick={onExpandToggle}>
+        <button className="icon-collasped" aria-expanded="true" onClick={onExpandToggle}>
           <svg height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none" /><path d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10z" /></svg>
         </button>;
     } else if (!showExpandCollapse) {
